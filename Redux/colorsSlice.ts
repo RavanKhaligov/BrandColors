44b97904da892a367644623e8553b20303ba7c9a
RavanKhaligov <112714179+RavanKhaligov@users.noreply.github.com>
2023-09-06 20:06:18 +0400

    Update colorsSlice.ts

--- Redux/colorsSlice.ts
+++ Redux/colorsSlice.ts
@@ -1,43 +1,59 @@
 'use client'
+import { colorSliceTypes, colorTypes } from "@/types";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import BrandsData from '../utils/brands.json';
 
-import { colorSliceTypes, colorTypes } from "@/types"
-import { PayloadAction, createSlice } from "@reduxjs/toolkit"
-import BrandsData from '../utils/brands.json'
+// Function to get selected brands from localStorage
+const getSelectedBrandsFromLocalStorage = () => {
+  if (typeof window !== "undefined") {
+    return JSON.parse(localStorage.getItem("selected") || "[]");
+  }
+  return [];
+};
 
-const initialState : colorSliceTypes = {
-    items: [...Object.values(BrandsData)].slice(0,10),
-    brandsArray:[...Object.values(BrandsData)],
-    brands:[...Object.values(BrandsData)],
-    selectedBrands: JSON.parse(localStorage.getItem("selected") || '[]'),
-    copied: '',
-    isShowing: false,
-    search: ''
-}
+const initialState: colorSliceTypes = {
+  items: [...Object.values(BrandsData)].slice(0, 10),
+  brandsArray: [...Object.values(BrandsData)],
+  brands: [...Object.values(BrandsData)],
+  selectedBrands: getSelectedBrandsFromLocalStorage(), // Initialize here
+  copied: '',
+  isShowing: false,
+  search: ''
+};
+
 export const colorsSlice = createSlice({
-    name:"colorsSlice",
-    initialState,
-    reducers:{
-        setBrands:(state,action : PayloadAction<colorTypes[]>) =>{
-            state.brands = action.payload
-        },
-        setSelectedBrands: (state,action : PayloadAction<string[]>) =>{
-            state.selectedBrands = action.payload
-            localStorage.setItem("selected",JSON.stringify(state.selectedBrands))
-        },
-        setCopied: (state,action: PayloadAction<string>) =>{
-            state.copied = action.payload
-        },
-        setIsShowing: (state,action:PayloadAction<boolean>) =>{
-            state.isShowing = action.payload
-        },
-        setSearch: (state,action: PayloadAction<string>)=>{
-            state.search = action.payload
-        },
-        setItems: (state,action: PayloadAction<colorTypes[]>) =>{
-            state.items = action.payload
-        }
-    }
-})
+  name: "colorsSlice",
+  initialState,
+  reducers: {
+    setBrands: (state, action: PayloadAction<colorTypes[]>) => {
+      state.brands = action.payload;
+    },
+    setSelectedBrands: (state, action: PayloadAction<string[]>) => {
+      state.selectedBrands = action.payload;
+      localStorage.setItem("selected", JSON.stringify(state.selectedBrands));
+    },
+    setCopied: (state, action: PayloadAction<string>) => {
+      state.copied = action.payload;
+    },
+    setIsShowing: (state, action: PayloadAction<boolean>) => {
+      state.isShowing = action.payload;
+    },
+    setSearch: (state, action: PayloadAction<string>) => {
+      state.search = action.payload;
+    },
+    setItems: (state, action: PayloadAction<colorTypes[]>) => {
+      state.items = action.payload;
+    },
+  },
+});
 
-export const {setBrands,setSelectedBrands,setCopied,setIsShowing,setSearch,setItems} = colorsSlice.actions
-export default colorsSlice.reducer
+export const {
+  setBrands,
+  setSelectedBrands,
+  setCopied,
+  setIsShowing,
+  setSearch,
+  setItems,
+} = colorsSlice.actions;
+
+export default colorsSlice.reducer;
